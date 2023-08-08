30eed302662d48e420ffb241711e86a997560638
Horgaring <dimarastvorsev@mail.ru>
2023-08-08 11:29:08 +0300

    Update MailRuResponse

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,5 @@
 ﻿
-using (Bot b = new Bot("5721864635:AAG4ZXAhPGqx304koGyThTO0XME0MxNdeMM",new FirefoxDriver()))
+using (Bot b = new Bot("TOKEN",new FirefoxDriver()))
 {
     b.Start();
     Console.ReadLine();
